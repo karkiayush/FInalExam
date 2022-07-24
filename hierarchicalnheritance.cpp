6fa64f71fba3c012aea2a7e82a57773ac5a5c2e4
karkiayush <karkiayush619@gmail.com>
2022-07-24 20:11:09 +0545

    first commit of jul 24

--- hierarchicalnheritance.cpp
+++ hierarchicalnheritance.cpp
@@ -1,34 +1,67 @@
 // When more than one class is inherited from base class is known as heirarchical inheritance
 #include <iostream>
+#include <string>
 using namespace std;
 class Base
 {
 public:
+    string name;
     int age;
     bool maritialStatus = true;
-    void getter()
+    void setter()
     {
-        cout << "Enter the age and of the young man " << endl;
-        cin >> age;
-
-        cout << "The age and maritial status of the young man is :  " << endl;
-        cout << age << endl
-             << maritialStatus << endl;
+        cout << "Enter the name,age and of the young man " << endl;
+        cin >> name >> age;
     }
-}
-}
-;
+};
 
 class Derived : public Base
 {
 public:
-    void setter(){
-
-    };
-    int main()
+    string n1, n2, n3, n4, n5;
+    void input()
     {
-        system("clear");
-        Derived d;
-        d.getter();
-        d.setter();
+        cout << "Enter the name of the child of old man  " << endl;
+        cin >> n1 >> n2 >> n3;
     }
+
+    void display()
+    {
+        cout << "\nInfo of old man" << endl;
+        cout << "name : " << name << endl;
+        cout << "age : " << age << endl;
+        cout << "maritial status : " << maritialStatus << endl;
+        cout << "\nThe info of old man's child " << endl;
+        cout << "child 1 : " << n1 << endl
+             << "child 2 : " << n2 << endl
+             << "child 3 : " << n3 << endl;
+    }
+};
+
+class Derived1 : public Base
+{
+public:
+    string address;
+    void fun()
+    {
+        cout << "Address of the old man : ";
+        cin >> address;
+    }
+
+    void output()
+    {
+        cout << "adress : " << address << endl;
+    }
+};
+
+int main()
+{
+    system("clear");
+    Derived1 d1;
+    Derived d2;
+    d1.setter();
+    d1.fun();
+    d2.input();
+    d2.display();
+    d1.output();
+}
