1c1e46ed5b6f9ae7256ad6bcb59bf7c49017bc27
karkiayush <karkiayush619@gmail.com>
2022-07-30 19:36:12 +0545

    2nd commit of july 30

---
+++ 17spring3b.cpp
@@ -0,0 +1,3 @@
+// create a class named "time" a constructor having hour, minute and second as an arguments is use to take two time data from user. The add function that takes two class objects an arguments adds them respectively then display the aggregragte result ? (Apply 60 secondds =1 mins and 60 mins = 1 hour)
+#include <iostream>
+using namepspace std;
