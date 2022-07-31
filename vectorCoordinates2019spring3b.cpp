a720594f0b7cbe00835c335661edaabb87e9f67d
karkiayush <karkiayush619@gmail.com>
2022-07-31 22:55:27 +0545

    3rd commit of july 31

--- vectorCoordinates2019spring3b.cpp
+++ vectorCoordinates2019spring3b.cpp
@@ -1,47 +1,38 @@
-// wap to input two vector coordinates from the "Base" class and derived class named  "derived" inherits all the properties of base class . derived class must contain a function named add_vector() that add the two vector input form the base class and finally display the result from a function display that is a friend with the base class
+/*wap to input two vector coordinates from the "Base" class and derived class named  "derived" inherits all
+the properties of base class . derived class must contain a function named add_vector() that add the two vector i
+nput form the base class and finally display the result from a function display that is a friend with the
+base class*/
 #include <iostream>
 using namespace std;
-class Base
+class derived;
+class base
 {
 public:
-    float e1, e2, sum;
-
-public:
-    void input()
+    int e1, e2, sum;
+    base()
     {
-        cout << "Enter the 2 vector coordinates " << endl;
+        cout << "enter the two vector coordinates" << endl;
         cin >> e1 >> e2;
     }
-    int assign()
-    {
-        sum = add_vector();
-        return 0;
-    }
-    int add_vector();
-    friend void disp(Base b);
+    friend void output(base &t, derived &d);
 };
-
-// Defination of frined function
-void disp(Base b)
+void output(base &t, derived &d)
 {
-    cout << "The sum is : " << b.sum << endl;
+    cout << "The sum of the vector coordinates " << t.e1 << " & " << t.e2 << " is : " << d.base::sum << endl;
 }
-
-class Derived : public Base
+class derived : public base
 {
 public:
-    float add_vector()
+    void add_vector()
     {
         sum = e1 + e2;
-        return sum;
     }
 };
 
 int main()
 {
-    Base b;
-    Derived d;
-    d.input();
-    b.add_vector();
-    disp(b);
+    base b;
+    derived d;
+    d.add_vector();
+    output(b, d);
 }
